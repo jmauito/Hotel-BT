837c61eeb4e1aa37bc850341ee8b7111f1736985
jmauito <jmauito@gmail.com>
2023-06-07 21:00:02 -0500

    Refresh the menu when change the page

--- index.js
+++ index.js
@@ -4,14 +4,25 @@ const routes = {
   "contact": "contact.html"
 }
 
+const refreshMenu = (path) => {
+  const menus = [ ...document.querySelectorAll('a.menu-item')];
+  menus.map(menu => {
+    console.log(menu.id);
+    const valueClass = menu.id == `menu-${path}` ? 'menu-item current' : 'menu-item';
+    menu.setAttribute('class', valueClass);
+  });
+  
+
+}
+
 const route = async() => {
   let path = window.location.hash.replace("#","");
-  console.log('path', path);
   if(path == '') path = 'home';
   const response = await fetch(routes[path]);
   const content = await response.text();
   const main = document.getElementById('main');
   main.innerHTML = content;
+  refreshMenu(path);
 }
 
 window.addEventListener('hashchange', route);
